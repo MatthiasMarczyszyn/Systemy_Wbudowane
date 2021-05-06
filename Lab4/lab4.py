63ef8fb0882719d73567c372905e52ba7f97aa35
MatthiasMarczyszyn <m.marczyszyn@gmail.com>
2021-05-06 15:40:39 +0200

    Add fourth lab program

---
+++ Lab4/lab4.py
@@ -0,0 +1,28 @@
+
+class CanonPrixima5():
+
+    def GetBlackInk(self):
+        pass
+
+    def GetColourInk(self):
+        pass
+
+    def GetPaperInk(self):
+        pass
+
+    def CheckBlackInkLevel(self):
+        pass
+
+    def CheckColourInkLeve(self):
+        pass
+
+    def PrintPage(self):
+        pass
+
+
+my_printer = CanonPrixima5()
+my_printer.CheckBlackInkLevel()
+my_printer.CheckColourInkLeve()
+my_printer.GetBlackInk()
+my_printer.GetColourInk()
+my_printer.PrintPage()
